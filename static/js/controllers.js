dc61df4f7407f70543f657bee72c2567181a44f0
Peter Allen <peter1t1allen@gmail.com>
2019-05-16 20:58:41 +0000

    adds markers and removes them

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,11 +1,10 @@
-//var markerlatlng = {lat: -25.363, lng: 131.044};
+
 var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
 
    var city = [];
     var latitude = [];
     var longitude = [];
     var markerArray = [];
-    var point;
 
 ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvider', '$httpProvider',
     function ($routeProvider, $locationProvider, $sceDelegateProvider, $httpProvider) {
@@ -15,15 +14,17 @@ ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvide
         controllerAs: 'wcontroller'
     });
 }]);
-function putPins(cities,lat,long) {
+function putPinsOnMap(cities,lat,long) {
+	setMapOnAll(null);
 
-    for (i = 0; i < cities.length; i++){
+    for (i = 0; i < city.length; i++){
     	var point = {lat: lat[i], lng: long[i]};
-        addMarker(point, map);       
+        addPin(point, map);       
     }
+    setMapOnAll(map);
 }
 
-function addMarker(location,map){
+function addPin(location,map){
 	
 	var marker = new google.maps.Marker({
 	position:location,
@@ -32,9 +33,7 @@ function addMarker(location,map){
 
 markerArray.push(marker);
 }
-    function clearMarkers() {
-        markerArray.setMapOnAll(null);
-      }
+ 
         function setMapOnAll(map) {
         for (var i = 0; i < markerArray.length; i++) {
           markerArray[i].setMap(map);
@@ -71,41 +70,38 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                 method: "GET",
                 url: '/api/v1/getWeather?zip=' + data
             }).then( function(response) {
-                if(which === 1) {
-     
-					
-                	//markerArray[0].setMap(null);
+                if(which === 1) {               
                     $scope.zip1City = response.data.city;
                     $scope.zip1Weather = response.data.weather;
                     city[0] = response.data.city;
                     latitude[0] = response.data.coord.lat;
                     longitude[0] = response.data.coord.lon; 
-                    point = {lat:latitude[0], lon:longitude[0]};
+                    
                   
                   
                     
                 } else if(which === 2) {
-                	//markerArray[1].setMap(null);
+                
                     $scope.zip2City = response.data.city;
                     $scope.zip2Weather = response.data.weather;
                     city[1] = response.data.city;
                     latitude[1] = response.data.coord.lat;
                     longitude[1] = response.data.coord.lon; 
-                    point = {lat:latitude[1], lon:longitude[1]};
+                  
 
                   
                 } else if(which === 3) {
-                	//markerArray[2].setMap(null);
+                	
                     $scope.zip3City = response.data.city;
                     $scope.zip3Weather = response.data.weather;
                     city[2] = response.data.city;
                     latitude[2] = response.data.coord.lat;
                     longitude[2] = response.data.coord.lon; 
-                    point = {lat:latitude[2], lon:longitude[2]};
+                    
                  
                     
                 } else if(which === 4) {
-               // markerArray[3].setMap(null);
+            
                     $scope.zip4City = response.data.city;
                     $scope.zip4Weather = response.data.weather;
                     city[3] = response.data.city;
@@ -113,7 +109,7 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
                     longitude[3] = response.data.coord.lon;
                   
                 } 
-                 putPins(city,latitude,longitude);
+                 putPinsOnMap(city,latitude,longitude);
             });       
     }; 
 }]);
